188fd8edc3364ad351d867f0a435f856c1eab4e3
DESKTOP2022\h195m <h195mic@gmail.com>
2023-06-15 15:01:07 +0900

    sdistするなど

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="PyImageSocket",
+    version="1.0.0",
+    description="My Library for send and receive image files using socket",
+    author="HaoriHakama678",
+    packages=["pyimagesocket", "pyimagesocket.image_socket"],
+    install_requires=["cv2"],
+)
