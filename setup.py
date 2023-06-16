bf3d6d460c4fbd8575569029a22f3f526c100214
DESKTOP2022\h195m <h195mic@gmail.com>
2023-06-16 11:15:22 +0900

    install_requiresの修正

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 
 setup(
     name="PyImageSocket",
-    version="1.0.0",
+    version="1.0.1",
     description="My Library for send and receive image files using socket",
     author="HaoriHakama678",
     packages=["pyimagesocket", "pyimagesocket.image_socket"],
-    install_requires=["cv2"],
+    install_requires=["opencv-python"],
 )
